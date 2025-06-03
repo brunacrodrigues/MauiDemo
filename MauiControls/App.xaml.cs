89f4995b8845632a7f7b2d699c5b7858a3602fbe
brcrodrigues96@gmail.com <brcrodrigues96@gmail.com>
2025-06-03 12:31:31 +0100

    Controls - Views

---
+++ MauiControls/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using MauiControls.Pages;
+
+namespace MauiControls
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            //var minhaPagina = new MinhaPagina();
+
+            //var navPage = new NavigationPage(minhaPagina);
+
+
+            //navPage.BarBackgroundColor = Colors.Yellow;
+            //navPage.BarTextColor = Colors.Blue;
+
+            var window = new Window(new MauiPresentationControls());
+
+
+            return window;
+        }
+    }
+}
