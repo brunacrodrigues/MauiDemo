2a0730abd4b000d0ae14fa73cb866ed0ad883a6e
brcrodrigues96@gmail.com <brcrodrigues96@gmail.com>
2025-06-05 15:49:06 +0100

    DataBinding I

---
+++ MauiDataBinding/MainPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using MauiDataBinding.Models;
+
+namespace MauiDataBinding
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        //    private void OnCounterClicked(object? sender, EventArgs e)
+        //    {
+        //        //count++;
+
+        //        //if (count == 1)
+        //        //    CounterBtn.Text = $"Clicked {count} time";
+        //        //else
+        //        //    CounterBtn.Text = $"Clicked {count} times";
+
+        //        //SemanticScreenReader.Announce(CounterBtn.Text);
+
+        //        var produto = new Produto
+        //        {
+        //            Nome = "Iphone 5",
+        //            Preco = 5000.00m,
+        //            Stock = 10
+        //        };
+
+        //        //Binding produtoBinding = new Binding();
+        //        //produtoBinding.Source = produto;
+        //        //produtoBinding.Path = "Nome";
+
+        //        Binding nomeBinding = new Binding
+        //        {
+        //            Source = produto,
+        //            Path = "Nome",
+        //            StringFormat = "Produto: {0}" 
+        //        };
+        //        lblNome.SetBinding(Label.TextProperty, nomeBinding);
+
+        //        //lblNome.SetBinding(Label.TextProperty, produtoBinding);
+
+        //        Binding precoBinding = new Binding
+        //        {
+        //            Source = produto,
+        //            Path = "Preco",
+        //            StringFormat = "Preço: {0}"
+        //        };
+        //        lblPreco.SetBinding(Label.TextProperty, precoBinding);
+
+        //        Binding stockBinding = new Binding
+        //        {
+        //            Source = produto,
+        //            Path = "Stock",
+        //            StringFormat = "Stock: {0}"
+        //        };
+        //        lblStock.SetBinding(Label.TextProperty, stockBinding);
+        //    }
+        //}
+    }
+}
---
+++ MauiDataBinding/Models/Produto.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiDataBinding.Models
+{
+    public class Produto
+    {
+        public string Nome { get; set; }
+
+        public decimal Preco { get; set; }
+
+        public int Stock { get; set; }
+    }
+}
