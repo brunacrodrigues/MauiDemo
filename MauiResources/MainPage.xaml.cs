65d6419d57c1d84cc5871ca84a5cf1a96e48021d
brcrodrigues96@gmail.com <brcrodrigues96@gmail.com>
2025-06-06 15:27:00 +0100

    Images, fonts, icons and other files

---
+++ MauiResources/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Text.Json;
+
+namespace MauiResources
+{
+    public partial class MainPage : ContentPage
+    {
+        Pessoa pessoa = new Pessoa();
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            // Load the Maui asset
+            await LoadMauiAsset();
+        }
+
+        async Task LoadMauiAsset()
+        {
+            using var stream = await FileSystem.OpenAppPackageFileAsync("demo.json");
+            using var reader = new StreamReader(stream);
+
+            var contents = reader.ReadToEnd();
+
+            var pessoa = JsonSerializer.Deserialize<Pessoa>(contents);
+
+            BindingContext = pessoa;
+        }
+    }
+
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+
+        public string Apelido { get; set; }
+
+        public int Idade { get; set; }
+
+        public string Foto { get; set; }
+    }
+}
